39872a15b663069929616a086e158e9ad4fc7bc2
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 13:15:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,20 +47,31 @@ bot.on('message', message => {
         console.log("makaos effectué");
     }
     
-        if (message.content === "-8ball") {
-    	var sayings =                                                          ["Certain",
-										"Oui",
-										"Non",
-										"Oui c'est sur Yagouroko-Kun le dit !",
-										"Non désoler",
-										"Je suis pas sur",
-										"C'est sur !",
-										"Dur a dire",];
+    if (message.content === "!8ball") {
+    	var sayings = ["It is certain",
+										"It is decidedly so",
+										"Without a doubt",
+										"Yes, definitely",
+										"You may rely on it",
+										"As I see it, yes",
+										"Most likely",
+										"Outlook good",
+										"Yes",
+										"Signs point to yes",
+										"Reply hazy try again",
+										"Ask again later",
+										"Better not tell you now",
+										"Cannot predict now",
+										"Concentrate and ask again",
+										"Don't count on it",
+										"My reply is no",
+										"My sources say no",
+										"Outlook not so good",
+										"Very doubtful"];
 
 			var result = Math.floor((Math.random() * sayings.length) + 0);
 			bot.reply(message, sayings[result]);
     }
-
 
 
 

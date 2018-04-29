585d17a4548030a1aa0a33e2998a11177c1e3615
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 13:21:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,32 +47,20 @@ bot.on('message', message => {
         console.log("makaos effectué");
     }
     
-    if (message.content === "!8ball") {
-    	var sayings = ["It is certain",
-										"It is decidedly so",
-										"Without a doubt",
-										"Yes, definitely",
-										"You may rely on it",
-										"As I see it, yes",
-										"Most likely",
-										"Outlook good",
-										"Yes",
-										"Signs point to yes",
-										"Reply hazy try again",
-										"Ask again later",
-										"Better not tell you now",
-										"Cannot predict now",
-										"Concentrate and ask again",
-										"Don't count on it",
-										"My reply is no",
-										"My sources say no",
-										"Outlook not so good",
-										"Very doubtful"];
-
-			var result = Math.floor((Math.random() * sayings.length) + 0);
-			bot.reply(message, sayings[result]);
-    }
-
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 
 

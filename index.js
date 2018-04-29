23149602b9d233e44b6a93799a0aa95d47907d44
Yagouroko <38819051+Yagouroko@users.noreply.github.com>
2018-04-29 13:05:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -47,27 +47,32 @@ bot.on('message', message => {
         console.log("makaos effectué");
     }
     
-    case "8ball":
-    let args = message.content.split(" ").slice(1);
-    let tte = args.join(" ")
-    if (!tte){
-       return message.reply("Merci de poser une question :8ball:")
-        
-        var replys = [
-            "Oui",
-            "Non",
-            "Je sais pas",
-            "Réessayer",
-            ];
-        
-        let reponse = (replys[Math.floor(Math.random() * replys.length)])
-        var bembed = new Discord.RichEmbed()
-        .setDescription(":8ball: 8ball")
-        .addField("Question", tte)
-        .addField("Réponse", reponse)
-        message.channel.sendEmbed(bembed)
-        
-}})
+        if (message.content === "!8ball") {
+    	var sayings = ["It is certain",
+										"It is decidedly so",
+										"Without a doubt",
+										"Yes, definitely",
+										"You may rely on it",
+										"As I see it, yes",
+										"Most likely",
+										"Outlook good",
+										"Yes",
+										"Signs point to yes",
+										"Reply hazy try again",
+										"Ask again later",
+										"Better not tell you now",
+										"Cannot predict now",
+										"Concentrate and ask again",
+										"Don't count on it",
+										"My reply is no",
+										"My sources say no",
+										"Outlook not so good",
+										"Very doubtful"];
+
+			var result = Math.floor((Math.random() * sayings.length) + 0);
+			bot.reply(message, sayings[result]);
+    }
+
 
 
 
